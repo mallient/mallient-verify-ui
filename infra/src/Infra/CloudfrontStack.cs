f84f2e8b6c3c20117f602423b0d631b1a7c14071
Michael Kelly <michael@mallient.com>
2026-03-07 23:03:27 -0600

    initial updated stacks

---
+++ infra/src/Infra/CloudfrontStack.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Amazon.CDK;
+using Constructs;
+
+namespace Infra
+{
+    public class CloudfrontStack : MallientStack
+    {
+        internal CloudfrontStack(Construct scope, string id, string envName, IStackProps props = null)
+            : base(scope, id, envName, props)
+        {
+
+        }
+    }
+}
---
+++ infra/src/Infra/MallientStack.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infra
+{
+    public class MallientStack : Stack
+    {
+        public string _envName { get; set; }
+
+        public MallientStack(Construct scope, string id, string envName, IStackProps props = null)
+                    : base(scope, id, props)
+        {
+            _envName = envName;
+        }
+    }
+}
