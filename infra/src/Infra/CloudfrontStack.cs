41b7406fe12db5be423f6fee2581b66f7f3d0f79
Michael Kelly <michael@mallient.com>
2026-03-09 11:42:09 -0500

    infra to put build artifact in s3 bucket

--- infra/src/Infra/CloudfrontStack.cs
+++ infra/src/Infra/CloudfrontStack.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Amazon.CDK;
+using Amazon.CDK.AWS.CloudFront;
+using Amazon.CDK.AWS.S3;
 using Constructs;
 
 namespace Infra
@@ -13,7 +15,19 @@ namespace Infra
         internal CloudfrontStack(Construct scope, string id, string envName, IStackProps props = null)
             : base(scope, id, envName, props)
         {
+            var siteBucket = new Bucket(this, $"MCSiteBucket-{envName}", new BucketProps
+            {
+                BucketName = $"mallient-concierge-ui-bucket-{envName}",
+                PublicReadAccess = false,
+                RemovalPolicy = RemovalPolicy.DESTROY,
+                AutoDeleteObjects = true,
+                BlockPublicAccess = BlockPublicAccess.BLOCK_ALL
+            });
 
+            var oai = new OriginAccessIdentity(this, $"OAI-{envName}", new OriginAccessIdentityProps
+            {
+                Comment = $"OAI for {envName}"
+            });
         }
     }
 }
--- infra/src/Infra/Program.cs
+++ infra/src/Infra/Program.cs
@@ -10,33 +10,28 @@ namespace Infra
         public static void Main(string[] args)
         {
             var app = new App();
-            new InfraStack(app, "InfraStack", new StackProps
+            string environmentInput = (string)app.Node.TryGetContext("env") ?? "development";
+            // Normalize environment name
+            string env = environmentInput.ToLower() switch
             {
-                // If you don't specify 'env', this stack will be environment-agnostic.
-                // Account/Region-dependent features and context lookups will not work,
-                // but a single synthesized template can be deployed anywhere.
+                "production" => "prod",
+                "development" => "dev",
+                "testing" => "test",
+                _ => environmentInput.ToLower() // fallback to original if unrecognized
+            };
 
-                // Uncomment the next block to specialize this stack for the AWS Account
-                // and Region that are implied by the current CLI configuration.
-                /*
+            Console.WriteLine($"Deploying to Environment: {env}");
+
+            string accountId = (string)app.Node.TryGetContext("accountId");
+            Console.WriteLine($"AccountID: {accountId}");
+
+            new CloudfrontStack(app, $"MallientVerify-{env}", env, new StackProps
+            {
                 Env = new Amazon.CDK.Environment
                 {
-                    Account = System.Environment.GetEnvironmentVariable("CDK_DEFAULT_ACCOUNT"),
-                    Region = System.Environment.GetEnvironmentVariable("CDK_DEFAULT_REGION"),
+                    Account = accountId,
+                    Region = "us-east-1"
                 }
-                */
-
-                // Uncomment the next block if you know exactly what Account and Region you
-                // want to deploy the stack to.
-                /*
-                Env = new Amazon.CDK.Environment
-                {
-                    Account = "123456789012",
-                    Region = "us-east-1",
-                }
-                */
-
-                // For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html
             });
             app.Synth();
         }
